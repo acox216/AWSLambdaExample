7a88a8336218609297a3f746a33d83161529d051
Alex Cox <alcox216@gmail.com>
2021-04-18 17:21:41 -0400

    Add project files.

---
+++ AWSLambdaExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AWSLambdaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
